6abf15b37f07a093672e5f332e50af43e6591727
Mirfozil <mirfoziln@gmail.com>
2025-07-31 11:16:36 +0500

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(num) {
+  if (typeof num !== 'number') {
+    throw new Error('Not implemented');
+  }
+  return Number(String(Math.abs(num)).split('').reverse().join(''));
 };
